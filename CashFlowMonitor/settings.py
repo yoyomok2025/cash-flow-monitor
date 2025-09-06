c5a09716ebbea2964ab7209c7b6c3e025ed37c2a
yoyomok2025 <yoyo.mok.003@gmail.com>
2025-09-06 16:24:14 +0800

    created datebase model

--- CashFlowMonitor/settings.py
+++ CashFlowMonitor/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'event',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -120,3 +121,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'event.User'
---
+++ event/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+    username= models.CharField(max_length=20,primary_key=True)
+    password=models.CharField(max_length=100)
+    created_date =models.DateField()
+    
+    class Meta:
+        db_table='user'
+    
+class Event(models.Model):
+    event_id=models.AutoField(primary_key=True)
+    event_type=models.CharField(max_length=4)
+    money=models.PositiveIntegerField()
+    time=models.DateField()
+    category=models.CharField(max_length=20)
+    created_time=models.DateTimeField()
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table='event'
